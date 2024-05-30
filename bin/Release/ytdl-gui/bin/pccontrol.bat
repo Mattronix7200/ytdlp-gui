21b15a9676e1d942b8f1b5a91796794271332778
Jakub Michalski <74902609+Mattronix7200@users.noreply.github.com>
2024-05-30 04:27:19 +0200

    Delete bin/Release/ytdl-gui directory

--- bin/Release/ytdl-gui/bin/pccontrol.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:loop
-tasklist /FI "IMAGENAME eq ytdl-gui.exe" 2>NUL | find /I /N "ytdl-gui.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-    taskkill /F /IM "ytdl-gui.exe" /T
-)
-timeout /t 1
-goto loop
